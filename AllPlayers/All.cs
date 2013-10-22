b49f871373a7ced8791cba7ae34aca09f22f409c
Simon311 <mkontent@gmail.com>
2013-10-22 21:35:49 +0400

    1.0.0.0

---
+++ AllPlayers/All.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using System.Linq;
+
+namespace All
+{
+	[ApiVersion(1, 14)]
+	public class All : TerrariaPlugin
+	{
+		public override Version Version
+		{
+			get { return new Version("1.0.0.0"); }
+		}
+
+		public override string Name
+		{
+			get { return "AllPlayers"; }
+		}
+
+		public override string Author
+		{
+			get { return "simon311"; }
+		}
+
+		public override string Description
+		{
+			get { return "Adds /doall."; }
+		}
+
+		public All(Main game)
+			: base(game)
+		{
+			Order = 0;
+		}
+
+		public override void Initialize()
+		{
+			Commands.ChatCommands.Add(new Command("doall", DoAll, "doall"));
+		}
+
+		public void DoAll(CommandArgs args)
+		{
+			if (args.Parameters.Count < 1)
+			{
+				args.Player.SendMessage("Usage: /doall [true] /command @all", Color.PaleGoldenrod);
+				args.Player.SendMessage("If true is present before the command - the command will affect you as well.", Color.PaleGoldenrod);
+				args.Player.SendMessage("You must use @all inside the command to reference all players.", Color.PaleGoldenrod);
+				return;
+			}
+			bool self = args.Parameters.Count != 0 && args.Parameters[0] == "true";
+			if (self) args.Parameters.RemoveAt(0);
+			string Command = String.Join(" ", args.Parameters);
+			TSPlayer[] Players = TShock.Players;
+			if (!self) Players = Players.Where(player => (player != null) && (player.Index != args.Player.Index)).ToArray();
+			if (Command.Contains("@all") && Players.Length > 0)
+			{
+				foreach (TSPlayer player in TShock.Players)
+				{
+					try
+					{
+						var cmd = Command.Replace("@all", player.Name);
+						Commands.HandleCommand(args.Player, cmd);
+					}
+					catch { }
+				}
+			}
+			else if (Players.Length == 0) args.Player.SendMessage("No players matched.", Color.PaleGoldenrod);
+			else args.Player.SendMessage("You must use @all inside your command.", Color.PaleGoldenrod);
+		}
+
+	}
+}
---
+++ AllPlayers/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("AllPlayers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AllPlayers")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a1a24503-4858-405e-a256-9cdb2c3d71c2")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
